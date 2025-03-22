2921b80aceedb454f0e5adf83dfc6bfc5264be0d
Thrashy190 <diego456.dlm77@gmail.com>
2025-03-22 02:37:29 -0600

    [Feature] Pages general features

---
+++ src/pages/Dashboard/Client/Clients.tsx
@@ -0,0 +1,17 @@
+const Clients = () => {
+  return (
+    <div>
+      <div className='flex justify-between'>
+        <h1 className='text-4xl text-primary font-bold'>Clientes</h1>
+      </div>
+      <div>
+        <span className='text-lg'>Agrega y Visualiza la informacion de tus clientes</span>
+      </div>
+      <div className='w-full'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Clients
